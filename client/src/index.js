aa315aa1705afe40dbc1e812cff0cfc222bdc6fa
Yoriqulov Oybek <hhoybek@gmail.com>
2022-08-05 11:38:33 +0500

    starting ticketing app

--- client/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import ReactDOM from 'react-dom'
-import App from './App'
-
-ReactDOM.render(<App/>, document.getElementById('root'))
