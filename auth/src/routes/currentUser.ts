b6d44ba05859323a268c51cc818599db3499fca2
Yoriqulov Oybek <hhoybek@gmail.com>
2022-08-07 00:49:21 +0500

    stuck on type augmentation typescript

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -1,16 +1,16 @@
 import {Request, Response, Router} from 'express'
-import jwt from 'jsonwebtoken'
+import {UserPayload} from '../middlewares/currentUser'
 
 const router = Router()
 
-router.get('/api/users/currentUser', (req: Request, res: Response) => {
-    if(!req.session?.jwt) return res.send({currentUser: null})
-    try {
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
-        return res.send({currentUser: payload})
-    } catch (e) {
-        return res.send({currentUser: null})
+declare module 'express-serve-static-core' {
+    export interface Request {
+        currentUser?: UserPayload
     }
+}
+
+router.get('/api/users/currentUser', (req: Request, res: Response) => {
+    res.send({currentUser: req.currentUser || null})
 })
 
 export {router as currentUserRoutes}
