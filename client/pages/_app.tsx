d561c89bdcacf7acdc7216117f2f9d854b34d802
Yoriqulov Oybek <hhoybek@gmail.com>
2022-08-07 16:45:37 +0500

    basic next app flow implemented

---
+++ client/pages/_app.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default function ({Component, pageProps}) {
+    return <Component {...pageProps}/>
+}
---
+++ client/pages/auth/signUp.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SignUp = () => {
+    return (
+        <form className='container'>
+            <label htmlFor="email">Email:</label>
+            <input type="email" className='form-control'/>
+        </form>
+    )
+}
+
+export default SignUp
