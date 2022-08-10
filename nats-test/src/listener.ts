c542b4a2796a92eae6e999dd03fb284f0447e6d2
Yoriqulov Oybek <hhoybek@gmail.com>
2022-08-10 15:11:18 +0500

    nast streaming first steps done

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,15 +1,40 @@
-import {connect, Stan} from 'node-nats-streaming'
+import {connect, Message, Stan} from 'node-nats-streaming'
+import {randomBytes} from 'crypto'
 
 console.clear()
 
-const stan: Stan = connect('ticketing', '123', {
-    url: 'http://localhost:4222'
-})
+const stan: Stan = connect(
+    'ticketing',
+    randomBytes(16).toString('hex'),
+    {
+        url: 'http://localhost:4222'
+    }
+)
 
 stan.on('connect', function () {
     console.log('Listener connected to NATS.')
-    const subscription = stan.subscribe('ticket:created')
-    subscription.on('message', function (message) {
-        console.log('Message received.')
+
+    stan.on('close', function () {
+        console.log('NATS client closed.')
+        process.exit()
+    })
+
+    const options = stan.subscriptionOptions()
+        .setManualAckMode(true)
+        .setDeliverAllAvailable()
+        .setDurableName('accounting-service')
+    const subscription = stan.subscribe(
+        'ticket:created',
+        'order-service-queue-group',
+        options
+    )
+    subscription.on('message', function (message: Message) {
+        console.log(
+            `Received message #${message.getSequence()} with data: ${message.getData()}`
+        )
+        message.ack()
     })
 })
+
+process.on('SIGINT', stan.close)
+process.on('SIGTERM', stan.close)
