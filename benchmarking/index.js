9d623bc22dc3c7d65bc2b9f13698ce888ba13fcd
Yoriqulov Oybek <hhoybek@gmail.com>
2022-08-13 23:24:10 +0500

    benchmarking done

---
+++ benchmarking/index.js
@@ -0,0 +1,41 @@
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
+const axios = require('axios');
+
+const cookie =
+    'session=eyJqd3QiOiJleUpoYkdjaU9pSklVekkxTmlJc0luUjVjQ0k2SWtwWFZDSjkuZXlKcFpDSTZJall5WmpkaVpERmtaRGt3Wm1JM1pqWTRNMk16WmpsaU5DSXNJbVZ0WVdsc0lqb2lkR1Z6ZEVCbmJXRnBiQzVqYjIwaUxDSnBZWFFpT2pFMk5qQTBNRGN3TmpWOS5NM2E2M1AzajVva0RQRXlnMnNqRnJSNFMxbVdCb2owUFZDbjQwY1pLOWxBIn0=';
+
+const doRequest = async () => {
+  const { data } = await axios.post(
+      `http://ticketing.dev/api/tickets`,
+      { title: 'ticket', price: 5 },
+      {
+        headers: { cookie },
+      }
+  );
+
+  await axios.put(
+      `http://ticketing.dev/api/tickets/${data.id}`,
+      { title: 'ticket', price: 10 },
+      {
+        headers: { cookie },
+      }
+  );
+
+  axios.put(
+      `http://ticketing.dev/api/tickets/${data.id}`,
+      { title: 'ticket', price: 15 },
+      {
+        headers: { cookie },
+      }
+  );
+
+  console.log('Request complete');
+};
+
+(async () => {
+  for (let i = 0; i < 200; i++) {
+    await doRequest();
+  }
+})();
