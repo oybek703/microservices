3ee7ab771b05d9639f8a8ca8ce0885a2133df3ec
Yoriqulov Oybek <hhoybek@gmail.com>
2022-08-16 12:53:54 +0500

    payments services basic setup done

---
+++ payments/src/index.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+import app from './app'
+import {natsWrapper} from './natsWrapper'
+
+async function start() {
+    if(!process.env.JWT_KEY) throw Error('JWT_KEY is not defined!')
+    if(!process.env.MONGO_URI) throw Error('MONGO_URI is not defined!')
+    if(!process.env.NATS_CLUSTER_ID) throw Error('NATS_CLUSTER_ID is not defined!')
+    if(!process.env.NATS_CLIENT_ID) throw Error('NATS_CLIENT_ID is not defined!')
+    if(!process.env.NATS_URL) throw Error('NATS_URL is not defined!')
+    try {
+        await natsWrapper.connect(
+            process.env.NATS_CLUSTER_ID,
+            process.env.NATS_CLIENT_ID,
+            process.env.NATS_URL
+        )
+        natsWrapper.client.on('close', function () {
+            console.log('NATS connection closed.')
+            process.exit()
+        })
+        process.on('SIGINT', natsWrapper.client.close)
+        process.on('SIGTERM', natsWrapper.client.close)
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('Successfully connected to MongoDB database!')
+    } catch (e) {
+        console.log(e)
+    }
+    app.listen(3000, () => {
+        console.log('Tickets service is running on port 3000...')
+    })
+}
+
+(async function () {
+    await start()
+})()
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,54 @@
+import {MongoMemoryServer} from 'mongodb-memory-server'
+import mongoose from 'mongoose'
+import {connect, connection} from 'mongoose'
+import {sign} from 'jsonwebtoken'
+
+jest.setTimeout(100000)
+
+jest.mock('../natsWrapper')
+
+declare global {
+    function signIn(): string
+}
+
+let mongo: MongoMemoryServer
+
+jest.mock('../natsWrapper')
+
+beforeAll(async function () {
+    process.env.JWT_KEY='test_key'
+    mongo = await MongoMemoryServer.create()
+    const mongoUri = await mongo.getUri()
+    await connect(mongoUri)
+})
+
+beforeEach(async function () {
+    jest.clearAllMocks()
+    const collections = await connection.db.collections()
+    for (const collection of collections) {
+        await collection.deleteMany({})
+    }
+})
+
+afterAll(async function () {
+    await mongo.stop()
+    await connection.close()
+})
+
+global.signIn = function () {
+    // Build JWT payload
+    const payload = {
+        id: new mongoose.Types.ObjectId().toHexString(),
+        email: 'test@gmail.com'
+    }
+    // Create JWT
+    const jwt = sign(payload, process.env.JWT_KEY!)
+    // Build session object
+    const session = {jwt}
+    // Turn that session into JSON
+    const sessionJSON = JSON.stringify(session)
+    // Take JSON and encode it as base64
+    const base64 = Buffer.from(sessionJSON).toString('base64')
+    // Return string cookie with encoded data
+    return `session=${base64}`
+}
