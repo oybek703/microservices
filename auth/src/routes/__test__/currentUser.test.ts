eee8ac9d549e2fc3576673e881796a7070879c51
Yoriqulov Oybek <hhoybek@gmail.com>
2022-08-18 09:25:01 +0500

    fixed tests

--- auth/src/routes/__test__/currentUser.test.ts
+++ auth/src/routes/__test__/currentUser.test.ts
@@ -5,7 +5,7 @@ it('should respond with details about the current user', async function () {
     const cookie = await global.signIn()
     const response = await request(app).get('/api/users/currentUser')
         .set('Cookie', cookie)
-        .send().expect(400)
+        .send().expect(200)
     expect(response.body.currentUser.email).toBe('test@gmail.com')
 })
 
